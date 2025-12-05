bdb2c4734c994ed5a8e77c932c9d2d6eb7189c56
onlingr <151378362+onlingr@users.noreply.github.com>
2025-12-06 00:49:14 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -13,8 +13,8 @@ root.render(
   <React.StrictMode>
     {/* 改回 HashRouter 以確保在預覽環境與靜態主機(如 GitHub Pages)能正常運作 */}
     {/* 若之後確定部署環境(如 Vercel)支援 Rewrite，可再改回 BrowserRouter */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
